3d048560743891ad30697c69efd79c47710d3927
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-08-06 22:48:15 -0300

    Requisito 6 concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 export default class Table extends Component {
   render() {
+    const header = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+      'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              {header.map((item) => <th key={ item }>{item}</th>)}
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
